07f1f126a93e153e3a5cba97a4436e4eccbaf55d
Роман Головенко <roma.golovenko.98@mail.ru>
2020-12-15 09:24:21 +0500

    соединение с бд

--- PlanPro/YMR.cs
+++ PlanPro/YMR.cs
@@ -12,6 +12,21 @@ namespace PlanPro
 {
     public partial class YMR : Form
     {
+
+        // Глобальные переменные
+        // Точка в хода в бд
+        string connStr = "server = osp74.ru; user = st_2_1;database = st_2_1;password = 33674763; port =33333";// Строка входа в бд
+        MySqlConnection conn_db;
+        private MySqlDataAdapter MyDA = new MySqlDataAdapter();
+        //Объявление BindingSource, основная его задача, это обеспечить унифицированный доступ к источнику данных.
+        private BindingSource bSource = new BindingSource();
+        //DataSet - расположенное в оперативной памяти представление данных, обеспечивающее согласованную реляционную программную 
+        //модель независимо от источника данных.DataSet представляет полный набор данных, включая таблицы, содержащие, упорядочивающие 
+        //и ограничивающие данные, а также связи между таблицами.
+        private DataSet ds = new DataSet();
+        //Представляет одну таблицу данных в памяти.
+        private DataTable table = new DataTable();
+
         public YMR()
         {
             InitializeComponent();
