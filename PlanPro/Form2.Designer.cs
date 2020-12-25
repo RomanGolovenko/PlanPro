34df737b2ca5e83f4319ea11c48c09cb5565b61e
Роман Головенко <roma.golovenko.98@mail.ru>
2020-12-25 17:03:45 +0500

    ворд

--- PlanPro/Form2.Designer.cs
+++ PlanPro/Form2.Designer.cs
@@ -34,6 +34,7 @@ namespace PlanPro
             this.button3 = new System.Windows.Forms.Button();
             this.button4 = new System.Windows.Forms.Button();
             this.button5 = new System.Windows.Forms.Button();
+            this.button6 = new System.Windows.Forms.Button();
             this.SuspendLayout();
             // 
             // button1
@@ -78,7 +79,7 @@ namespace PlanPro
             // 
             // button5
             // 
-            this.button5.Location = new System.Drawing.Point(674, 351);
+            this.button5.Location = new System.Drawing.Point(500, 348);
             this.button5.Name = "button5";
             this.button5.Size = new System.Drawing.Size(113, 53);
             this.button5.TabIndex = 4;
@@ -86,11 +87,23 @@ namespace PlanPro
             this.button5.UseVisualStyleBackColor = true;
             this.button5.Click += new System.EventHandler(this.button5_Click);
             // 
+            // button6
+            // 
+            this.button6.Location = new System.Drawing.Point(674, 348);
+            this.button6.Name = "button6";
+            this.button6.Size = new System.Drawing.Size(113, 53);
+            this.button6.TabIndex = 5;
+            this.button6.Tag = "1";
+            this.button6.Text = "Перейти в\r\nдокумент";
+            this.button6.UseVisualStyleBackColor = true;
+            this.button6.Click += new System.EventHandler(this.button6_Click);
+            // 
             // Form2
             // 
             this.AutoScaleDimensions = new System.Drawing.SizeF(6F, 13F);
             this.AutoScaleMode = System.Windows.Forms.AutoScaleMode.Font;
             this.ClientSize = new System.Drawing.Size(841, 450);
+            this.Controls.Add(this.button6);
             this.Controls.Add(this.button5);
             this.Controls.Add(this.button4);
             this.Controls.Add(this.button3);
@@ -109,5 +122,6 @@ namespace PlanPro
         private System.Windows.Forms.Button button3;
         private System.Windows.Forms.Button button4;
         private System.Windows.Forms.Button button5;
+        private System.Windows.Forms.Button button6;
     }
 }
--- PlanPro/Form2.cs
+++ PlanPro/Form2.cs
@@ -7,6 +7,7 @@ using System.Linq;
 using System.Text;
 using System.Threading.Tasks;
 using System.Windows.Forms;
+using Word = Microsoft.Office.Interop.Word;
 
 namespace PlanPro
 {
@@ -16,6 +17,11 @@ namespace PlanPro
         {
             InitializeComponent();
         }
+        private Word.Application wordapp;
+        private Word.Documents worddocuments;
+        private Word.Document worddocument;
+       // private Word.Paragraphs wordparagraphs;
+        private Word.Paragraph wordparagraph;
         // кнопка УМР
         private void button1_Click(object sender, EventArgs e)
         {
@@ -46,6 +52,72 @@ namespace PlanPro
         {
             //Закрытие формы
             Close();
+        }
+        //кнопка открытия WORD 
+        private void button6_Click(object sender, EventArgs e)
+        {
+            int i = Convert.ToInt32(((Button)(sender)).Tag);
+            switch (i)
+            {
+                case 1:
+
+                    worddocument.Content.ParagraphFormat.Alignment =
+ Word.WdParagraphAlignment.wdAlignParagraphCenter;
+                    worddocument.Content.ParagraphFormat.LeftIndent =
+                     worddocument.Content.Application.CentimetersToPoints((float)2);
+                    worddocument.Content.ParagraphFormat.RightIndent =
+                     worddocument.Content.Application.CentimetersToPoints((float)1);
+
+                    //Вставляем в документ 4 параграфа
+                    object oMissing = System.Reflection.Missing.Value;
+                    worddocument.Paragraphs.Add(ref oMissing);
+                    worddocument.Paragraphs.Add(ref oMissing);
+                    worddocument.Paragraphs.Add(ref oMissing);
+                    worddocument.Paragraphs.Add(ref oMissing);
+                    //Переходим к первому добавленному параграфу
+                    wordparagraph = worddocument.Paragraphs[2];
+                    Word.Range wordrange = wordparagraph.Range;
+                    //Добавляем таблицу в начало второго параграфа
+                    Object defaultTableBehavior =
+                     Word.WdDefaultTableBehavior.wdWord9TableBehavior;
+                    Object autoFitBehavior =
+                     Word.WdAutoFitBehavior.wdAutoFitWindow;
+                    Word.Table wordtable1 = worddocument.Tables.Add(wordrange, 5, 5,
+                      ref defaultTableBehavior, ref autoFitBehavior);
+                    //Сдвигаемся вниз в конец документа
+                    object unit;
+                    object extend;
+                    unit = Word.WdUnits.wdStory;
+                    extend = Word.WdMovementType.wdMove;
+                    wordapp.Selection.EndKey(ref unit, ref extend);
+                    //Вставляем таблицу по месту курсора
+                    Word.Table wordtable2 = worddocument.Tables.Add(
+                      wordapp.Selection.Range, 4, 4, ref defaultTableBehavior,
+                    ref autoFitBehavior);
+                    //Меняем стили созданных таблиц
+                    Object style = "Классическая таблица 1";
+                    wordtable1.set_Style(ref style);
+                    style = "Сетка таблицы 3";
+                    Object applystyle = true;
+                    wordtable2.set_Style(ref style);
+                    wordtable2.ApplyStyleFirstColumn = true;
+                    wordtable2.ApplyStyleHeadingRows = true;
+                    wordtable2.ApplyStyleLastRow = false;
+                    wordtable2.ApplyStyleLastColumn = false;
+                    break;
+                case 2:
+                    Object saveChanges = Word.WdSaveOptions.wdPromptToSaveChanges;
+                    Object originalFormat = Word.WdOriginalFormat.wdWordDocument;
+                    Object routeDocument = Type.Missing;
+                    wordapp.Quit(ref saveChanges,
+                                 ref originalFormat, ref routeDocument);
+                    wordapp = null;
+                    break;
+               
+                default:
+                    Close();
+                    break;
+            }
         }
     }
 }
